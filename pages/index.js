b5c3b88b7bf78f3e938eb8ececa81ea7eb8631dd
sospedra <sospedra.r@gmail.com>
2017-01-24 08:33:40 +0100

    feat: Add initial pages and link them

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+
+export default () => (
+  <div>
+     <h2>Welcome home!</h2>
+     <Link href='/menu'>To menu</Link>
+  </div>
+)
---
+++ pages/menu/index.js
@@ -0,0 +1,10 @@
+import css from 'next/css'
+
+const style = css({
+  color: '#f95d75'
+})
+
+export default () => (
+  <div className={style}>This is the menu</div>
+)
+
