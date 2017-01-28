336313898e00cf216addf5a4d248d1cb5e29f842
sospedra <sospedra.r@gmail.com>
2017-01-28 16:01:30 +0100

    refactor: Favour pods arch

--- app/initialize.js
+++ app/initialize.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import App from 'components/App'
+import Home from 'pods/home'
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.querySelector('#app'))
+  ReactDOM.render(<Home />, document.querySelector('#app'))
 })
