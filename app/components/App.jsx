9daf9ba6bcb0517f285da1b0265dd46f36fa4f17
sospedra <sospedra.r@gmail.com>
2017-01-28 15:12:18 +0100

    chore: Init new brunch with React

---
+++ app/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div id="content">
+        <h1>&nbsp;</h1>
+        <h2>Welcome!</h2>
+        <ul>
+          <li><a href="http://brunch.io">Brunch homepage</a></li>
+          <li><a href="https://facebook.github.io/react/">React.js homepage</a></li>
+        </ul>
+      </div>
+    );
+  }
+}
