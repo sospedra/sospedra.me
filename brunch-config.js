79acc3db57c5577c7e30af255faa0451c8ba2b81
sospedra <sospedra.r@gmail.com>
2017-01-29 20:51:52 +0100

    feat: Add helmet

--- brunch-config.js
+++ brunch-config.js
@@ -16,6 +16,7 @@ module.exports = {
     babel: {
       presets: ['es2015', 'react']
     },
+
     htmlPages: {
       compileAssets: true
     }
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const path = require('path')
 const compression = require('compression')
+const helmet = require('helmet')
 
 const app = express()
 
+app.use(helmet())
 app.use(compression())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
