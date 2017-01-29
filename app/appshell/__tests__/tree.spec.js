b7f02800a2064c3bf2120564d87bb2da8dca522c
sospedra <sospedra.r@gmail.com>
2017-01-29 20:32:13 +0100

    test: Update appshell test

--- app/appshell/__tests__/tree.spec.js
+++ app/appshell/__tests__/tree.spec.js
@@ -5,10 +5,13 @@ import { shallow } from 'enzyme'
 import Appshell from '../index'
 
 describe('Appshell wrapper suite', () => {
-  const wrapper = shallow(<Appshell />)
+  const wrapper = shallow(<Appshell>
+    <div>A</div>
+    <div>B</div>
+  </Appshell>)
 
-  it('should have a text with the pod name', () => {
-    expect(wrapper.find('p').text()).toBe('Appshell')
+  it('should have as many children as sent by props', () => {
+    expect(wrapper.children().length).toBe(2)
   })
 
   it('should match the snapshot', () => {
