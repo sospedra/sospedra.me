2f98b0dfd1ce5f57790672e6b9509166251f43a7
sospedra <sospedra.r@gmail.com>
2017-03-12 18:23:42 +0100

    feat: Add sprite background

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,18 +2,27 @@ import Inferno from 'inferno'
 import { Link } from 'inferno-router'
 import './Home.css'
 
+const BG_SPRITE = 'images/street-sprite.svg'
+
 const Home = () => (
-  <div className='Home'>
-    <nav>
-      <Link to='/about'>About</Link>
-    </nav>
-    <div className='Home-header'>
-      <h2>Welcome to sospedra.me</h2>
-    </div>
-    <p className='Home-intro'>
-      To get started, edit <code>src/Home.js</code> and save to reload.
-    </p>
-    <img src={process.env.PUBLIC_URL + '/images/react.png'} />
+  <div>
+    <main className='menu'>
+      <h1 className='title'>
+        Rub&eacute;n Sospedra
+      </h1>
+      <h2 className='sub-title'>
+        javascript hacker * front-end developer
+      </h2>
+    </main>
+
+    <figure
+      className='sprite move-slide'
+      alt='retrowave-skyline-moving-left-as-background'
+      style={{
+        animationDuration: `${window.innerWidth / 138}s`,
+        backgroundImage: `url(${process.env.PUBLIC_URL}/${BG_SPRITE})`
+      }}
+    />
   </div>
 )
 
