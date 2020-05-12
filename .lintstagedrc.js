cce6b32cae7940410d30c5af48d9c42f4dee80f1
Rubén Sospedra <hello@sospedra.me>
2020-05-12 11:54:05 +0200

    fixup! fixup! f

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,7 +3,7 @@ const resize = require('./internals/resize')
 module.exports = {
   '*.{ts,tsx,css,md}': () => 'prettier --write',
   '*.{gif,jpg,jpeg,tiff,png}': (filenames) => {
-    filenames.map((filename) => resize(filename))
+    filenames.forEach((filename) => resize(filename))
     return []
   },
 }
--- internals/resize.js
+++ internals/resize.js
@@ -9,28 +9,6 @@ const writeFile = promisify(fs.writeFile)
 
 sharp.cache(false)
 
-// module.exports = async (e) => {
-//   console.log('run', e)
-//   for (const paper of await readdir(root)) {
-//     for (const file of await readdir(join(root, paper))) {
-//       if (file.match(/[\/.](gif|jpg|jpeg|tiff|png)$/i)) {
-//         const image = sharp(join(root, paper, file))
-
-//         image
-//           .metadata()
-//           .then(({ width }) => (width > 672 ? image.resize(672) : image))
-//           .then((data) => data.jpeg().toBuffer())
-//           .then((data) => {
-//             writeFile(
-//               join(root, paper, file.replace(/\.[^.]+$/, '.jpeg')),
-//               data,
-//             )
-//           })
-//       }
-//     }
-//   }
-// }
-
 module.exports = (filename) => {
   const image = sharp(filename)
 
