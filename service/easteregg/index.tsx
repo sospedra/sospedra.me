8edf8dc0012b4ff12778e9834850d93e94a65985
Rubén Sospedra <hello@sospedra.me>
2020-05-15 12:00:32 +0200

    fixup! feat: Add touch screen support (#10)

--- service/easteregg/index.tsx
+++ service/easteregg/index.tsx
@@ -8,7 +8,7 @@ const Tap = dynamic(() => import('./Tap'))
 
 const EasterEgg: React.FC<{}> = (props) => {
   const [isActive, setIsActive] = useState(false)
-  const [isTapVisible, setIsTapVisible] = useState(true)
+  const [isTapVisible, setIsTapVisible] = useState(false)
 
   useMousetrap([
     [
