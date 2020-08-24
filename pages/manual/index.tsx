3166cc5e3dea24f312fa59d0e0f97d8b3f389224
Rubén Sospedra <hello@sospedra.me>
2020-08-24 13:32:03 +0200

    feat: Add intro manual

--- pages/manual/index.tsx
+++ pages/manual/index.tsx
@@ -2,9 +2,32 @@ import { NextPage } from 'next'
 import css from './manual.module.css'
 import Shell from 'components/Shell'
 import SpriteBust from 'components/Sprite/Bust'
+import SpriteManual from 'components/Sprite/Manual'
+import Link from 'components/Link'
 
 export const MANUAL_DESC =
   'How to work with Rubén Sospedra. A manual of instructions. What I value, how I look at problems, what are my blind spots, and how to build trust with me.'
+
+const PICTO_HEIGHT = { style: { height: '30%' } }
+const Pictogram: React.FC<{
+  left: React.ReactNode
+  right: React.ReactNode
+  style?: React.CSSProperties
+}> = ({ left, right, style = {} }) => {
+  return (
+    <div
+      className='flex flex-row items-center w-full my-4 border border-current rounded-lg'
+      style={style}
+    >
+      <div className='flex items-center justify-center flex-1 h-full'>
+        {left}
+      </div>
+      <div className='flex items-center justify-center flex-1 h-full'>
+        {right}
+      </div>
+    </div>
+  )
+}
 
 const Manual: NextPage<{}> = () => {
   return (
@@ -26,10 +49,39 @@ const Manual: NextPage<{}> = () => {
         <img src='/sospedra.png' className={css.logo} />
       </section>
       <section className={css.page}>
-        <p>thinks u need</p>
+        <div className='flex flex-row' {...PICTO_HEIGHT}>
+          <SpriteManual name='support' />
+          <Pictogram
+            left={<SpriteManual name='gameboy' className='p-8' />}
+            right={<SpriteManual name='handshake' className='p-8' />}
+          />
+        </div>
+        <Pictogram
+          {...PICTO_HEIGHT}
+          left={<SpriteManual name='lost' />}
+          right={
+            <Link url='/' className='h-full'>
+              <SpriteManual name='home' />
+            </Link>
+          }
+        />
       </section>
       <section className={css.page}>
-        <h2>How to</h2>
+        <div
+          className='flex flex-row items-center justify-between'
+          {...PICTO_HEIGHT}
+        >
+          <SpriteManual name='demons' />
+          <SpriteManual name='mobius' />
+          <SpriteManual name='triangle' />
+        </div>
+        <div
+          className='flex flex-row items-center justify-between'
+          {...PICTO_HEIGHT}
+        >
+          <SpriteManual name='insert' />
+          <SpriteManual name='count' />
+        </div>
       </section>
     </Shell>
   )
