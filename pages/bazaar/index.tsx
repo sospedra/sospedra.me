300680d008d1758731ba13f6bf0d1ad7733e115f
Rubén Sospedra <hello@sospedra.me>
2020-08-26 10:46:00 +0200

    feat: Add bazaar scroll

--- pages/bazaar/index.tsx
+++ pages/bazaar/index.tsx
@@ -1,9 +1,12 @@
+import { useState, useEffect } from 'react'
 import { NextPage } from 'next'
+import cn from 'classnames'
 import { STACK_DESC } from 'pages/stack'
 import { SERVE_DESC } from 'pages/serve'
 import { REWRITE_DESC } from 'pages/rewrite'
 import { USES_DESC } from 'pages/uses'
 import { MANUAL_DESC } from 'pages/manual'
+import { useScroll } from 'service/scroll'
 import Link, { LinkBack } from 'components/Link'
 import Shell from 'components/Shell'
 import SpriteMountain from 'components/Sprite/Mountain'
@@ -17,93 +20,116 @@ const BAZAAR_DESC = 'Gallery of my featured projects'
 
 const Bazaar: NextPage = () => {
   const nextConstellation = useConstellation()
+  const [isHidden, setIsHidden] = useState(false)
+  const [intent, setIntent] = useState(false)
+  const scrollRef = useScroll((e) => {
+    return e.target.scrollTop > 300 ? setIntent(true) : setIntent(false)
+  })
+
+  useEffect(() => {
+    if (intent !== isHidden) {
+      setIsHidden(intent)
+    }
+  }, [intent, isHidden])
 
   return (
     <Shell title='Bazaar' description={BAZAAR_DESC} canonical='/bazaar'>
-      <div className={css.bazaar}>
-        <Link url='/'>
-          <LinkBack>Home</LinkBack>
-        </Link>
-        <h1 className='font-serif text-4xl text-cyan-300'>Bazaar</h1>
-        <p>{BAZAAR_DESC}</p>
+      <div className='w-full h-screen overflow-auto' ref={scrollRef}>
+        <div className={css.bazaar}>
+          <Link url='/'>
+            <LinkBack>Home</LinkBack>
+          </Link>
+          <h1 className='font-serif text-4xl text-cyan-300'>Bazaar</h1>
+          <p>{BAZAAR_DESC}</p>
 
-        <ul className={css.list}>
-          <li>
-            <Link className={css.title} url='/stack'>
-              the stack
-            </Link>
-            <p>{STACK_DESC}</p>
-          </li>
-          <li>
-            <Link className={css.title} url='/manual'>
-              user guide manual
-            </Link>
-            <p>{MANUAL_DESC}</p>
-          </li>
-          <li>
-            <Link className={css.title} url='/uses'>
-              uses
-            </Link>
-            <p>{USES_DESC}</p>
-          </li>
-          <li>
-            <Link className={css.title} url='/serve'>
-              serve assets
-            </Link>
-            <p>{SERVE_DESC}</p>
-            <Constellation name={nextConstellation()} />
-          </li>
-          <li>
-            <Link className={css.title} url='/rewrite'>
-              rewrites
-            </Link>
-            <p>{REWRITE_DESC}</p>
-          </li>
-          <li>
-            <Cheatcodes className={css.title} />
-            <p>wait wat?</p>
-            <Constellation name={nextConstellation()} />
-          </li>
-          <li>
-            <External href='https://rfm.sospedra.me' className={css.title}>
-              rfm
-            </External>
-            <p>
-              Track OSS <b>requests for maintainers</b>. Find any project
-              calling for collaborators.
-            </p>
-          </li>
-          <li>
-            <External href='https://reinput.sospedra.me' className={css.title}>
-              reinput
-            </External>
-            <p>A React Native TextInput with material style 😎</p>
-            <Constellation name={nextConstellation()} />
-          </li>
-          <li>
-            <External href='https://spg.sospedra.me' className={css.title}>
-              spg
-            </External>
-            <p>
-              Secure passwords that humans can read 🗝
-              <br />
-              Generate passwords that are semantically correct. The passwords
-              are more secure the longer they are. They don't need symbols or
-              special characters at all. We end up using cryptic passwords that
-              are impossible to type. This generator uses NLP technology to
-              create semantically meaningful passwords.
-            </p>
-          </li>
-          <li>
-            <External href='https://keycodes.sospedra.me' className={css.title}>
-              which key code
-            </External>
-            <p>Which keys map to what keyboard code?</p>
-            <Constellation name={nextConstellation()} />
-          </li>
-        </ul>
+          <ul className={css.list}>
+            <li>
+              <Link className={css.title} url='/stack'>
+                the stack
+              </Link>
+              <p>{STACK_DESC}</p>
+            </li>
+            <li>
+              <Link className={css.title} url='/manual'>
+                user guide manual
+              </Link>
+              <p>{MANUAL_DESC}</p>
+            </li>
+            <li>
+              <Link className={css.title} url='/uses'>
+                uses
+              </Link>
+              <p>{USES_DESC}</p>
+            </li>
+            <li>
+              <Link className={css.title} url='/serve'>
+                serve assets
+              </Link>
+              <p>{SERVE_DESC}</p>
+              <Constellation name={nextConstellation()} />
+            </li>
+            <li>
+              <Link className={css.title} url='/rewrite'>
+                rewrites
+              </Link>
+              <p>{REWRITE_DESC}</p>
+            </li>
+            <li>
+              <Cheatcodes className={css.title} />
+              <p>wait wat?</p>
+              <Constellation name={nextConstellation()} />
+            </li>
+            <li>
+              <External href='https://rfm.sospedra.me' className={css.title}>
+                rfm
+              </External>
+              <p>
+                Track OSS <b>requests for maintainers</b>. Find any project
+                calling for collaborators.
+              </p>
+            </li>
+            <li>
+              <External
+                href='https://reinput.sospedra.me'
+                className={css.title}
+              >
+                reinput
+              </External>
+              <p>A React Native TextInput with material style 😎</p>
+              <Constellation name={nextConstellation()} />
+            </li>
+            <li>
+              <External href='https://spg.sospedra.me' className={css.title}>
+                spg
+              </External>
+              <p>
+                Secure passwords that humans can read 🗝
+                <br />
+                Generate passwords that are semantically correct. The passwords
+                are more secure the longer they are. They don't need symbols or
+                special characters at all. We end up using cryptic passwords
+                that are impossible to type. This generator uses NLP technology
+                to create semantically meaningful passwords.
+              </p>
+            </li>
+            <li>
+              <External
+                href='https://keycodes.sospedra.me'
+                className={css.title}
+              >
+                which key code
+              </External>
+              <p>Which keys map to what keyboard code?</p>
+              <Constellation name={nextConstellation()} />
+            </li>
+          </ul>
+        </div>
       </div>
-      <aside className={css.offscreen}>
+      <aside
+        className={cn(css.offscreen, {
+          ['translate-y-full']: isHidden,
+        })}
+      >
         <div className={css.car}>
           <SpriteCar />
         </div>
