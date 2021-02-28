c3374b624993844c0ca9a326f8de1dfb3fc9a46c
Rubén Sospedra <rsospedra@cameo.com>
2021-02-28 09:28:50 +0100

    feat: Add heading links Manual

--- components/Manual/Step.tsx
+++ components/Manual/Step.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import css from './step.module.css'
 
 const Step: React.FC<{ title: string; number: number }> = (props) => {
+  const slug = `chapter-${props.number}`
   return (
     <div className={css.step}>
-      <h2 className={css.title}>
-        <span className={css.number}>{props.number}</span>
-        {props.title}
+      <h2 id={slug} className={css.title}>
+        <a href={`#${slug}`}>
+          <span className={css.number}>{props.number}</span>
+          {props.title}
+        </a>
       </h2>
       {props.children}
     </div>
